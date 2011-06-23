220caf3c1bb057e5378837887b09a816cde145c5
araufernando90@gmail.com <araufernando90@gmail.com@a5b86c80-ae45-fb61-1733-ce9bd8370360>
2011-06-23 23:59:55 +0000

    holis

---
+++ trunk/Chess/src/chess/business/Board.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package chess.business;
+
+import java.util.ArrayList;
+
+public class Board {
+    private ArrayList squares;
+    private ArrayList moves;
+    private int dimensions;
+    public Board(){
+
+    }
+    public boolean move(Move move){
+
+    }
+    public boolean undoLastMove(){
+
+    }
+    public void changePiece(Position position, Piece piece){
+
+    }
+    public Piece getPieceAt(Position position){
+        
+    }
+}
+
+
