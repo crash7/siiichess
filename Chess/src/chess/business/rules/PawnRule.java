8514339ec0d68730607ba98f45cd7a037c67e03d
lean33@gmail.com <lean33@gmail.com@a5b86c80-ae45-fb61-1733-ce9bd8370360>
2011-06-30 18:41:35 +0000

    Modificado el PawnRule, falta el comer pasando

--- Chess/src/chess/business/rules/PawnRule.java
+++ Chess/src/chess/business/rules/PawnRule.java
@@ -2,8 +2,8 @@
 package chess.business.rules;
 
 import chess.business.board.Board;
+import chess.business.board.PieceMove;
 import chess.business.util.Move;
-import chess.business.util.Position;
 import chess.business.pieces.King;
 import chess.business.pieces.Piece;
 import java.util.List;
@@ -16,63 +16,63 @@ public class PawnRule extends PieceRule {
 
     
     public boolean makeMove(Move move, Board board, King king, List oppiece) {
-        int x=0;
-        int y=0;
-        x=move.getDestination().getX()-move.getSource().getX();
-        y=move.getDestination().getX()-move.getSource().getY();
-        
-        Piece piezatemporal;
-        if (this.getPiece().getColor()=='w')
+        if (isValidMove(move) && pathIsClear(move, board))
         {
-            if (x==-2)
+            board.move(new PieceMove(this.getPiece(),board.getPieceAt(move.getDestination()), move));
+            if (endsInCheck(board, king, oppiece))
             {
-                if (y==0 && this.getPiece().getMovesCount()==0)
-                {
-                    piezatemporal=board.getPieceAt(new Position(move.getSource().getX()-1,move.getSource().getY()));
-                    if (piezatemporal==null)
-                      {
-                        piezatemporal=board.getPieceAt(move.getDestination());
-                        if (piezatemporal==null)return true;
-                      }
-                }
+                board.undoLastMove();
+                return false;
+            }
+            else return true;
                 
-            }
-            else
-            {
-                if (x==-1 && y>=-1 && y<=1)
-                {
-                    if (y==0)
-                    {
-                        piezatemporal=board.getPieceAt(move.getDestination());
-                        if (piezatemporal==null)return true;   
-                    }   
-                    else
-                    {
-                        piezatemporal=board.getPieceAt(move.getDestination());
-                        if (piezatemporal==null)
-                        {
-                            piezatemporal=board.getPieceAt(new Position (move.getSource().getX(),move.getDestination().getY()));
-                            if (piezatemporal!=null)
-                            {
-                                if (!piezatemporal.sameColour(this.getPiece()) && piezatemporal.sameType(this.getPiece()))return true;
-                                /*Falta verificar que:
-                                * a) El movimiento anterior es el que dejo a esa pieza ahi.
-                                * b) Fue un doble avance del peon.
-                                */
-                            }
-                         }
-                            else
-                            {
-                                if (!piezatemporal.sameColour(this.getPiece()))return true;
-                                
-                            }
-                        
-                    }
-                }
-            }
+            
         }
-        
-        return false;
+        else return false;
     }
     
-}
+    private boolean isValidMove(Move move) {
+        int x=move.getDestination().getX()-move.getSource().getX();
+        int y=move.getDestination().getY()-move.getSource().getY();
+        y=Math.abs(y);
+        if (this.getPiece().isWhite())
+        {
+            if (x==-1 && (y==1 || y==0))return true;
+            else return false;
+        }
+        else
+        {
+             if (x==1 && (y==1 || y==0))return true;
+            else return false;
+        }
+     }
+     private boolean pathIsClear(Move move, Board board) {
+         Piece piezatemporal=null;
+         if (move.getSource().getY()!=move.getDestination().getY())
+             {
+                 piezatemporal=board.getPieceAt(move.getDestination());
+                 if (piezatemporal!=null)
+                 {
+                     if (piezatemporal.sameColour(this.getPiece()))return false;
+                     else return true;
+                 }
+                 else
+                 {
+                     return false;//comer pasando.
+                 }
+                 
+             }
+             else
+             {
+                  piezatemporal=board.getPieceAt(move.getDestination());
+                 if (piezatemporal!=null)return false;
+                 else return true;
+                 
+             }
+         }
+        
+         
+     }
+     
+    
+
