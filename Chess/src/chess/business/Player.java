cec9ec1840d032fb945c64825100f72bd83c0d67
christianmusa <christianmusa@a5b86c80-ae45-fb61-1733-ce9bd8370360>
2011-07-02 21:16:02 +0000

    Fix 16

--- Chess/src/chess/business/Player.java
+++ Chess/src/chess/business/Player.java
@@ -13,60 +13,61 @@ public class Player {
 	public Player(String name, char color) {
 		this.color = color;
 		this.name = name;
-		
+
 	}
 
 	public void setName(String name) {
 		this.name = name;
-		
+
 	}
 
 	public String getName() {
 		return this.name;
-		
+
 	}
 
 	public void setColor(char color) {
 		this.color = color;
-		
+
 	}
 
 	public char getColor() {
 		return this.color;
-		
+
 	}
 
 	public King getKing() {
 		return this.king;
-		
+
 	}
 
 	public void addPiece(Piece piece) {
 		this.pieces.add(piece);
-                if (piece.getKeyname()=='k') {
-                    this.king=(King)piece;
-                }
-		
+		if (piece instanceof King) {
+			this.king = (King) piece;
+			
+		}
+
 	}
 
 	public List getPieces() {
 		return this.pieces;
-		
+
 	}
-	
+
 	public boolean equals(Object o) {
 		return this.color == ((Player) o).getColor();
-		
+
 	}
-	
+
 	public boolean isWhite() {
 		return this.color == 'W' || this.color == 'w';
-		
+
 	}
-	
+
 	public boolean isBlack() {
 		return this.color == 'B' || this.color == 'b';
-		
+
 	}
 
 }
