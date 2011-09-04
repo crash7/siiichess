f2daea1a812f84d4cb8ab544310e7719ef24e834
christianmusa <christianmusa@a5b86c80-ae45-fb61-1733-ce9bd8370360>
2011-09-04 20:49:02 +0000

    Implentaciones copadas

--- Chess/src/UI/BoardGUI.java
+++ Chess/src/UI/BoardGUI.java
@@ -1,8 +1,15 @@
 package UI;
 
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Graphics;
 import java.awt.GridLayout;
+import java.awt.Insets;
 import java.awt.Point;
+
+import javax.swing.BorderFactory;
 import javax.swing.JPanel;
+import javax.swing.border.Border;
 
 class BoardGUI extends JPanel {
 
@@ -12,14 +19,20 @@ class BoardGUI extends JPanel {
     }
     
     private void init() {
+    	CellGUI temp;
     	setLayout(new GridLayout(8, 8));
     	boolean dark = true;
     	for(int i=0; i < 8; i++) {
     		dark = !dark;
     		for(int j=0; j < 8; j++) {
-    			add(new CellGUI(i, j, dark));
-    			dark = !dark;
     			
+    			temp = new CellGUI(i, j, dark);
+    			add(temp);
+    			dark = !dark;
+    			if(i == 0 || i == 7) {
+    				temp.setPiece(PieceRepositoryGUI.get().getPiece("kb"));
+    				
+    			}
     		}
     		
     	}
