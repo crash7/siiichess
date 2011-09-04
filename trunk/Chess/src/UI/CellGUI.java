c48c0b244bcddd45d42b818c4916bb7f27de597c
christianmusa <christianmusa@a5b86c80-ae45-fb61-1733-ce9bd8370360>
2011-09-04 18:52:51 +0000

    CellGUI

--- trunk/Chess/src/UI/CellGUI.java
+++ trunk/Chess/src/UI/CellGUI.java
@@ -1,19 +1,49 @@
 package UI;
 
+import java.awt.Color;
+
 import javax.swing.JLabel;
 
 public class CellGUI extends JLabel {
-	private int color;
+	private boolean darkCell;
+	private PieceGUI piece;
 	
-	public CellGUI(boolean black) {
-		
+	public CellGUI(boolean dark) {
+		darkCell = dark;
+		if(darkCell) {
+			setBackground(Color.BLUE);
+			
+		} else {
+			setBackground(Color.WHITE);
+			
+		}
 		
 	}
 	
 	public CellGUI() {
-		color = 'b';
+		this(false);
+		piece = null;
 		
 	}
 	
-
+	public void setPiece(PieceGUI p) {
+		piece = p;
+		
+	}
+	
+	public PieceGUI getPiece() {
+		return piece;
+		
+	}
+	
+	public boolean isEmpty() {
+		return piece == null;
+		
+	}
+	
+	public boolean isDark() {
+		return darkCell;
+		
+	}
+	
 }
